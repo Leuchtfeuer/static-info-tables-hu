39870526b5d259f8dfe76855b0b8fe17beb101c4
Max Rösch <github@elbebass.com>
2021-08-30 16:26:22 +0200

    [CODE] Codestyles

--- class.ext_update.php
+++ class.ext_update.php
@@ -3,9 +3,11 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the "Static Info Tables (HU)" extension for TYPO3 CMS.
+ * This file is part of the "Static Info Tables (AR)" extension for TYPO3 CMS.
+ *
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
+ *
  * Florian Wessels <f.wessels@Leuchtfeuer.com>, Leuchtfeuer Digital Marketing
  */
 
