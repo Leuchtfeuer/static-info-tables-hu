3142dccbcb27e1e369edab0b584b729b047994b3
Max Rösch <github@elbebass.com>
2022-07-22 09:03:40 +0200

    [TASK] Adapt dependecies for latest TYPO3 major version

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,9 @@
 <?php
-defined('TYPO3_MODE') || die;
+defined('TYPO3_MODE') || die();
 
-call_user_func(
-    function($extensionKey) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-            sprintf('@import \'EXT:%s/Configuration/TypoScript/Extbase/setup.typoscript\'', $extensionKey)
-        );
-    }, \Leuchtfeuer\StaticInfoTablesHu\Extension::EXTENSION_KEY
-);
+call_user_func(function ($extensionKey) {
+    // Deprecated since 10.0 @see https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $extensionKey . '/Configuration/TypoScript/Extbase/setup.typoscript">'
+    );
+}, \Leuchtfeuer\StaticInfoTablesHu\Extension::EXTENSION_KEY);
